89e83ec9967ecab85b3ef8f8cee95132af8b705b
PC <fatd@mail.bg>
2018-11-09 20:06:25 +0000

    Fully working Login/Register System

--- login.php
+++ login.php
@@ -26,13 +26,17 @@
             Password: <input type="password" name="password" placeholder="Please enter a password" value="<?php if(isset($_POST['LogIn'])){echo $_POST['password'];} ?>" required/><br><br>
         </div>
         <div class = "button">
-            <input type="submit" name="LogIn" value="LogIn"/>
+            <input type="submit" name="LogIn" value="Log In"/>
         </div>
+        <p>
+<!--            <a href="#" class="btn btn-link btn-sm" role="button" name="pwdreset">Don't remember your password?</a>-->
+            <input type="submit" name="pwdreset" value="Reset Pass">
+        </p>
         <p>
             Don't have an account? <a href="register.php">Register Now</a>
         </p>
         <p>
-            Forgotten Password? <a href="newPass.php">Reset Password</a>
+            Don't like your password? <a href="newPass.php">Reset Password</a>
         </p>
     </form>
 </div>
@@ -41,3 +45,86 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
+<?php
+    $email = isset($_POST["email"])? $_POST["email"]: "";
+    $pwd = isset($_POST["password"])? $_POST["password"]: "";
+    $invalidinfo = 0;
+
+    if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        $invalidinfo = 1;
+    }
+
+    if(empty($pwd) || preg_match('/\s/', $pwd))
+    {
+        $invalidinfo = 1;
+    }
+
+    if($invalidinfo === 0)
+    {
+        //Connect to the database
+        $host = "devweb2018.cis.strath.ac.uk";
+        $user = "cs312groupk";
+        $password = "aeCh1ang9ahm";
+        $dbname = "cs312groupk";
+        $conn = new mysqli($host, $user, $password, $dbname);
+
+        if(isset($_POST['LogIn'])) {
+            $encryptedPassword = md5($pwd);
+            $sql = "SELECT * FROM `cs312groupk`.`users` WHERE `email` = '$email' AND `password` = '$encryptedPassword'";
+            $result = $conn->query($sql);
+
+            if(mysqli_num_rows($result) == 1){
+               $_SESSION['email'] = $email;
+                header("Location: mainPage.php");
+            }else{
+                echo "Wrong email or password.";
+            }
+        }
+
+        if(isset($_POST["pwdreset"]))
+        {
+            $email_found = 0;
+            $sql = "SELECT * FROM users";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0)
+            {
+                while ($row = $result->fetch_assoc())
+                {
+                    if($row["email"] === $email)
+                    {
+                        $email_found = 1;
+                        break;
+                    }
+                }
+            }
+
+            if($email_found === 1)
+            {
+                $newpwd = rand();
+                $new_encr_pwd = md5($newpwd);
+                $message = "Your new password is $newpwd";
+                mail($email, "Password Reset - Group K's Website", $message);
+                $sql = "UPDATE users SET password = '$new_encr_pwd' WHERE email = '$email'";
+
+                if ($conn->query($sql) === TRUE)
+                {
+                    echo "<div>Password updated successfully!</div>";
+                    echo "<div>Your new password has been sent to: $email</div>";
+                }
+                else
+                {
+                    echo "<div>Error updating record: " . $conn->error . "</div>";
+                }
+            }
+
+            else
+            {
+                echo "<div>No such email found</div>";
+            }
+        }
+
+
+    }
+?>
--- register.php
+++ register.php
@@ -18,21 +18,71 @@
         <p>In order to create an account, please fill in the following</p>
     </div>
     <form method="post" action="register.php">
+        <div class = "name">
+            Name: <input type="text" name="name" placeholder="Please enter a name."  required/><br><br>
+        </div>
         <div class = "email">
             Email: <input type="text" name="email" placeholder="Please enter an email." value="<?php if(isset($_POST['Register'])){echo $_POST['email'];} ?>" required/><br><br>
-        </div class = "password">
+        </div>
         <div class = "password">
             Password: <input type="password" name="password" placeholder="Please enter a password" value="<?php if(isset($_POST['Register'])){echo $_POST['password'];} ?>" required/><br><br>
         </div>
+        <div class = "institution">
+            Institution: <input type="text" name="institute" placeholder="Please enter a institute"  required/><br><br>
+        </div>
         <div class="button">
-            <input type="submit" name="Register" value="Register"/>
+            <input type="submit" name="Register" value="Register"/><br><br>
         </div>
         <p>
             Already registered? <a href="login.php">Login here</a>
         </p>
     </form>
 </div>
+<div>
+    <?php
+    /**
+     * Created by IntelliJ IDEA.
+     * User: Hristo Petkov
+     * Date: 10/23/2018
+     * Time: 6:10 PM
+     */
 
+    session_start();
+
+    if(isset($_POST['Register'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $institute = $_POST['institute'];
+        $encryptedPassword = md5($password);
+
+        //Connect to the Database
+        $serverName = "devweb2018.cis.strath.ac.uk";
+        $userName = "cs312groupk";
+        $password = "aeCh1ang9ahm";
+        $dbname = $userName;
+        $conn = new mysqli($serverName, $userName, $password, $dbname);
+        if($conn->connect_error){
+            die("Fialed to connect to database");
+        }
+
+        $sql = "SELECT * FROM `cs312groupk`.`users` WHERE `email` = '$email'";
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $result = $conn->query($sql);
+            if(mysqli_num_rows($result) == 1){
+                echo "User already exists";
+            }else{
+                $sql2 = "INSERT INTO `cs312groupk`.`users` (`name`,`password`,`email`,`id`,`institute`) VALUES ('$name', '$encryptedPassword', '$email', NULL, '$institute');";
+                $result2 = $conn->query($sql2);
+                $_SESSION['email'] = $email;
+                header("Location: mainPage.php");
+            }
+        }else{
+            echo "Invalid email! Please enter a valid email";
+        }
+    }
+    ?>
+</div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
