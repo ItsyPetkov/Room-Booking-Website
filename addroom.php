0fc5c8275515a92d42cb080ca3b18c9da7b82ab7
Julia Tennant CS2016 <julia.tennant.2016@uni.strath.ac.uk>
2018-11-14 17:21:30 +0000

    Added consistent Bootstrap styling and javascript for error messages enabled.

--- addroom.php
+++ addroom.php
@@ -1,71 +1,183 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Add Room </title>
+    <!-- Bootstrap core CSS -->
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Clockpicker CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-clockpicker.min.css">
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <?php include("includes/header.php"); ?>
 <h1>Add A Room Bitch </h1>
 <body>
 <div>
-    <form enctype="multipart/form-data" action="addroom.php" method="post">
-        <p>  Room Number:  <input type="string"  name="rnumber" value="<?php if(isset($_POST['rnumber'])) {echo $_POST['rnumber'];} ?>" required />  </p>
-        <p>  Building  <input type="string"  name="building" value="<?php if(isset($_POST['building'])) {echo $_POST['building'];} ?>" required />  </p>
-        //add institue from sessions???? if logged in as a owner rather than user get inst from db
-        <p>  Institute  <input type="string"  name="inst" value="<?php if(isset($_POST['inst'])) {echo $_POST['inst'];} ?>" required />  </p>
-        <p>  Capacity  <input type="int"  name="cap" value="<?php if(isset($_POST['cap'])) {echo $_POST['cap'];} ?>" required />  </p>
-        <p>  Hours Available (From)  <input type="time"  name="from" value="<?php if(isset($_POST['from'])) {echo $_POST['from'];} ?>" required />  </p>
-        <p>  Hours Available (To)  <input type="time"  name="to" value="<?php if(isset($_POST['to'])) {echo $_POST['to'];} ?>" required />  </p>
+    <form class="needs-validation" novalidate enctype="multipart/form-data" action="addroom.php" method="post">
+        <div class="form-group row">
+            <label for="num" class="col-sm-2 col-form-label">Room number:</label>
+            <div class="col-md-6 mb-3">
+                <input type="text" class="form-control" name="num" id="num" placeholder="Enter room number." autocomplete="off" required>
+                <div class="invalid-feedback">
+                    Please include a room number.
+                </div>
+            </div>
+        </div>
 
-        <br><input type="submit" value="Submit" />
+        get buildings from database!!
+        <div class="form-group row">
+            <label for="location" class="col-sm-2 col-form-label">Location</label>
+            <div class="col-md-6 mb-3">
+                <select class="custom-select" id="location" name="location" required>
+                    <option value="">Choose the location</option>
+                    <option value="building1" <?php if($location == "building1") echo 'selected="selected"'; ?>>buildingOne</option>
+                    <option value="building2" <?php if($location == "building2") echo 'selected="selected"'; ?>>buildingTwo</option>
+                    <option value="building3" <?php if($location == "building3") echo 'selected="selected"'; ?>>buildingThree</option>
+                </select>
+                <div class="invalid-feedback">
+                    Please, choose a location!
+                </div>
+            </div>
+        </div>
+
+        get institute from database!!
+        <div class="form-group row">
+            <label for="institute" class="col-sm-2 col-form-label">Institute</label>
+            <div class="col-md-6 mb-3">
+                <select class="custom-select" id="institute" name="institute" required>
+                    <option value="">Choose the institute</option>
+                    <option value="building1" <?php if($location == "building1") echo 'selected="selected"'; ?>>buildingOne</option>
+                    <option value="building2" <?php if($location == "building2") echo 'selected="selected"'; ?>>buildingTwo</option>
+                    <option value="building3" <?php if($location == "building3") echo 'selected="selected"'; ?>>buildingThree</option>
+                </select>
+                <div class="invalid-feedback">
+                    Please, choose a location!
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="capacity" class="col-sm-2 col-form-label">Room capacity</label>
+            <div class="col-md-6 mb-3">
+                <input type="number" class="form-control" name="capacity" id="capacity" placeholder="Enter room capacity" onkeypress="return isNumber(event)" min="1" max="500" value="<?php if(isset($_POST['capacity'])) echo $_POST['capacity']; ?>" required>
+                <div class="invalid-feedback">
+                    Please, choose room capacity between 1 and 500!
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="timestart" class="col-md-2 col-form-label">Time range start</label>
+            <div class="col-md-4 mb-3">
+                <div class="input-group clockpicker">
+                    <input type="text" class="form-control" name="timestart" id="timestart" placeholder="00:00" pattern="(09|10|11|12|13|14|15|16):[0-5][0-9]" autocomplete="off" value="<?php if(isset($_POST['timestart'])) echo $_POST['timestart']; ?>" required>
+                    <div class="invalid-feedback">
+                        Please, enter the time of the meeting (between 09:00 and 17:00)!
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <div class="form-group row">
+                <label for="timeend" class="col-md-2 col-form-label">Time range end</label>
+                <div class="col-md-4 mb-3">
+                    <div class="input-group clockpicker">
+                        <input type="text" class="form-control" name="timeend" id="timeend" placeholder="00:00" pattern="(09|10|11|12|13|14|15|16):[0-5][0-9]" autocomplete="off" value="<?php if(isset($_POST['timeend'])) echo $_POST['timeend']; ?>" required>
+                        <div class="invalid-feedback">
+                            Please, enter the time of the meeting (between 09:00 and 17:00)!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <button type="submit" class="btn btn-outline-primary" name="submitStep2">Submit</button>
     </form>
-
-
-
-
-
-
-    <?php
-    if(!isset($_POST['rnumber']) || !isset($_POST['building']) || !isset($_POST['inst']) || !isset($_POST['cap']) || !isset($_POST['from']) || !isset($_POST['to']) ) {
-        echo '<p>Please complete all fields!</p>';
-    } else {
-
-
+<?php
 //info for adding to db
-        $rnumber = $_POST['rnumber'];
-        $building = $_POST['building'];
-        $institue = $_POST['inst'];
-        $capacity = $_POST['cap'];
-        $hfrom = $_POST['from'];
-        $hto = $_POST['to'];
+        $rnumber = isset($_POST["num"])? $_POST["num"]: "";
+        $building = isset($_POST["location"])? $_POST["location"]: "";
+        $institue = isset($_POST["institute"])? $_POST["institute"]: "";
+        $capacity = isset($_POST["capacity"])? $_POST["capacity"]: "";
+        $hfrom = isset($_POST["timestart"])? $_POST["timestart"]: "";
+        $hto = isset($_POST["timeend"])? $_POST["timeend"]: "";
 
 
 //connect to the database
-        $host = "devweb2018.cis.strath.ac.uk";
-        $user = "cs312groupk";
-        $password = "aeCh1ang9ahm";
-        $dbname = "cs312groupk";
-        $conn = new mysqli($host, $user, $password, $dbname);
-        if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);//FIXME only show error during debugging
+        include("includes/config.php");
+        include("includes/db.php");
+        if ($db->connect_error) die("Connection failed: " . $db->connect_error);//FIXME only show error during debugging
 
 
         $sql = "INSERT INTO `rooms` (`id`, `roomNumber`, `building`, `institute`, `capacity`, `hoursAvailableS`, `hoursAvailableE`) VALUES (NULL, '$rnumber', '$building', '$institue', '$capacity', '$hfrom', '$hto' )";
 
         echo "<p></p>";
 
-        if ($conn->query($sql) === TRUE) {
+        if ($db->query($sql) === TRUE) {
             echo "Room " . $rnumber . " in " . $institue . " has been added!";
         }
 
+        $db->close();
+?>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap-clockpicker.min.js"></script>
 
-        $conn->close();
+    <script>
+        $('.clockpicker').clockpicker({
+            donetext: 'Done'
+        });
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
 
-    }
-    ?>
+        // Enables only numeric input
+        function isNumber(evt) {
+            evt = (evt) ? evt : window.event;
+            var charCode = (evt.which) ? evt.which : evt.keyCode;
+            if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+                return false;
+            }
+            return true;
+        }
+        // Function for changing the tabs from the form step wizard
+        function selectStep(evt, tabName, flag) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("nav-link");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(tabName).style.display = "block";
 
+            if (evt.currentTarget.type == "button") {
+                tablinks = document.getElementsByClassName(tabName);
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className += " active";
+                }
+            } else {
+                evt.currentTarget.className += " active";
+            }
+        }
 
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
+    </script>
 </body>
-
-
-
 </html>
