43a18cfc74dfdc51ef6f9f7d631e0457807fa70c
Julia Tennant CS2016 <julia.tennant.2016@uni.strath.ac.uk>
2018-11-16 19:54:20 +0000

    basic ass user home, still need to add buttons to book new room etc. plus error fixing

---
+++ userhome.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rnb16141
+ * Date: 14/11/2018
+ * Time: 14:22
+ */
+session_start();
+
+$id = $_SESSION['id'];
+$name = $_SESSION['name'];
+$email = $_SESSION['email'];
+
+
+
+
+include("includes/config.php");
+include("includes/db.php");
+if ($db->connect_error) die("Connection failed: " . $db->connect_error);
+
+// Issue the query
+$sql = "SELECT * FROM `bookings` WHERE `bookings`.`user_id` = '$id'";
+$result = $db->query($sql);
+
+
+if ($result->num_rows > 0) {
+
+
+    echo "
+    <table>
+        <tr>
+            <th></th>
+            <th>Meeting Name</th>
+            <th>Time</th>
+            <th>Date</th>
+            <th>Institution</th>
+            <th>Room No.</th>
+        </tr>
+  ";
+    // Filling the table
+    while ($row = $result->fetch_assoc()) {
+
+$sql2 = "SELECT * FROM `rooms` WHERE `id` = '".$row["room_id"]."'";
+$result2 = $db->query($sql2);
+        while ($row2 = $result2->fetch_assoc()) {
+            $roomno = $row2["roomNumber"];
+        }
+
+        echo "<tr>\n";
+        echo "<td></td>";
+        echo "<td>" . $row["meeting_name"] . "</td>";
+        echo "<td>" . $row["start_time"] . " - " . $row["end_time"] . "</td>";
+        echo "<td>" . $row["institution"]  . "</td>";
+        echo "<td>" . $roomno  . "</td>";
+
+        echo "</tr>\n";
+    }
+    }
+    else {
+        echo "if youre seeing this the associated user ID doesnt have any current booking that match it, add some via phpmyadmin until theres more fucntionality lol, also if theres 3 errors up top it means that the session thing is confused. either manually set the varaibles in the code or navigate to this through the login/register pages with no institute";
+
+}
+    // Disconnect
+    $db->close();
+
+    ?>
