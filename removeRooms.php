916cf411f63e9db8788c5994aa14cbbe9ebbc7ac
Steven Brown CS2016 <steven.brown.2016@uni.strath.ac.uk>
2018-11-16 21:09:23 +0000

    Upload New File

---
+++ removeRooms.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>View Rooms</title>
+    <h1>Title</h1>
+<body>
+<div>
+  Are you sure you want ot delete this room?
+    <form action="viewRooms.php" method="post">
+        <td><button type=\"submit\" name = \"orderButton\" value = yes>Yes</button></td>
+        <td><button type=\"submit\" name = \"orderButton\" value = no>No</button></td>
+</div>
+</body>
+</html>
