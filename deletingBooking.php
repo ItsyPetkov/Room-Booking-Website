3979860713423c0de736c4a518de620425ddd0a1
Steven Brown CS2016 <steven.brown.2016@uni.strath.ac.uk>
2018-11-18 17:48:57 +0000

    Upload New File

---
+++ deletingBooking.php
@@ -0,0 +1,27 @@
+<?php
+// connect to mysql
+$host = "devweb2018.cis.strath.ac.uk";
+$user = "cs312groupk";
+$pass = "aeCh1ang9ahm";
+$dbname = "cs312groupk";
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error)
+{
+    die("Connection failed : ".$conn->connect_error); // Remove once working!!!
+}
+
+$chosenRoom = $_POST["confirm"];
+
+if($chosenRoom != "no")
+{
+    // Issue the query
+    $sql = "SELECT * FROM `bookings` WHERE id = '$chosenRoom'";
+}
+
+// Disconnect
+$conn->close();
+
+header("Location: https://devweb2018.cis.strath.ac.uk/~gxb16190/groupk/index.php");
+exit();
+?>
