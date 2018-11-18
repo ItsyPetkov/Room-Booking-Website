b2d1a97bef0dc054fd969b1e04f7e59d619fbc5e
Julia Tennant CS2016 <julia.tennant.2016@uni.strath.ac.uk>
2018-11-18 15:10:30 +0000

    Owner home now lookin sexier too, more css/functionality to be added though ALSO needs to have remove room completed so it can link to something that works

--- ownerhome.php
+++ ownerhome.php
@@ -1,11 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home </title>
+
+
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<?php include("includes/header.php");
+session_start();
+?>
+<h1>Home lmao </h1>
+<body>
+    <?php
 /**
  * Created by IntelliJ IDEA.
  * User: rnb16141
  * Date: 14/11/2018
  * Time: 14:23
  */
-session_start();
+
 
 $name = $_SESSION['name'];
 $email = $_SESSION['email'];
@@ -22,8 +36,8 @@ if ($db->connect_error) die("Connection failed: " . $db->connect_error);
 $sql = "SELECT * FROM `rooms` WHERE `institute` = '$inst'";
 $result = $db->query($sql);
 
-if ($result->num_rows > 0)
-{ echo"
+if ($result->num_rows > 0) {
+    echo "
     <table>
         <tr>
             <th></th>
@@ -39,18 +53,30 @@ if ($result->num_rows > 0)
     while ($row = $result->fetch_assoc()) {
         echo "<tr>\n";
         echo "<td></td>";
+        echo "<td>" . $row["id"] . "</td>";
         echo "<td>" . $row["roomNumber"] . "</td>";
         echo "<td>" . $row["building"] . "</td>";
         echo "<td>" . $row["institute"] . "</td>";
         echo "<td>" . $row["capacity"] . "</td>";
         echo "<td>" . $row["hoursAvailableS"] . " - " . $row["hoursAvailableE"] . "</td>";
+        echo "<form action = 'removeRooms.php' method = 'post'>";
+        echo "<td><button name ='remove'  value=" . $row["id"] . "/>Remove Room</button></td></form>";
 
         echo "</tr>\n";
     }
+}
+    else{
+        echo "fuck aw here lads";
+}
     // Disconnect
     $db->close();
 
-    echo" <a href=\"addroom.php\" class=\"button\">Add a Room</a>";
-        }?>
+        ?>
+
+    <?php
+    echo "<form action = 'addroom.php' method = 'post'>";
+    echo "<button name ='add'/>Add Room</button></form>";
+    ?>
+</body>
 
 
