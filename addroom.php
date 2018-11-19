2f44e918e3bb241ce1ccc7c0baf0509ecff81f86
Julia Tennant CS2016 <julia.tennant.2016@uni.strath.ac.uk>
2018-11-19 15:50:30 +0000

    Made some changes in addroom, still needs some fixing (getting buildings for location from database)

--- addroom.php
+++ addroom.php
@@ -9,8 +9,10 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap-clockpicker.min.css">
     <!-- Custom CSS -->
     <link href="css/style.css" rel="stylesheet">
-    <?php include("includes/header.php"); ?>
-<h1>Add A Room Bitch </h1>
+    <?php
+    session_start();
+    include("includes/header.php");?>
+    <h1>Add A Room Bitch </h1>
 <body>
 <div>
     <form class="needs-validation" novalidate enctype="multipart/form-data" action="addroom.php" method="post">
@@ -40,21 +42,7 @@
             </div>
         </div>
 
-        get institute from database!!
-        <div class="form-group row">
-            <label for="institute" class="col-sm-2 col-form-label">Institute</label>
-            <div class="col-md-6 mb-3">
-                <select class="custom-select" id="institute" name="institute" required>
-                    <option value="">Choose the institute</option>
-                    <option value="building1" <?php if($location == "building1") echo 'selected="selected"'; ?>>buildingOne</option>
-                    <option value="building2" <?php if($location == "building2") echo 'selected="selected"'; ?>>buildingTwo</option>
-                    <option value="building3" <?php if($location == "building3") echo 'selected="selected"'; ?>>buildingThree</option>
-                </select>
-                <div class="invalid-feedback">
-                    Please, choose a location!
-                </div>
-            </div>
-        </div>
+
 
         <div class="form-group row">
             <label for="capacity" class="col-sm-2 col-form-label">Room capacity</label>
@@ -78,45 +66,45 @@
             </div>
         </div>
 
-            <div class="form-group row">
-                <label for="timeend" class="col-md-2 col-form-label">Time range end</label>
-                <div class="col-md-4 mb-3">
-                    <div class="input-group clockpicker">
-                        <input type="text" class="form-control" name="timeend" id="timeend" placeholder="00:00" pattern="(09|10|11|12|13|14|15|16):[0-5][0-9]" autocomplete="off" value="<?php if(isset($_POST['timeend'])) echo $_POST['timeend']; ?>" required>
-                        <div class="invalid-feedback">
-                            Please, enter the time of the meeting (between 09:00 and 17:00)!
-                        </div>
+        <div class="form-group row">
+            <label for="timeend" class="col-md-2 col-form-label">Time range end</label>
+            <div class="col-md-4 mb-3">
+                <div class="input-group clockpicker">
+                    <input type="text" class="form-control" name="timeend" id="timeend" placeholder="00:00" pattern="(09|10|11|12|13|14|15|16):[0-5][0-9]" autocomplete="off" value="<?php if(isset($_POST['timeend'])) echo $_POST['timeend']; ?>" required>
+                    <div class="invalid-feedback">
+                        Please, enter the time of the meeting (between 09:00 and 17:00)!
                     </div>
                 </div>
             </div>
+        </div>
         <button type="submit" class="btn btn-outline-primary" name="submitStep2">Submit</button>
     </form>
-<?php
-//info for adding to db
-        $rnumber = isset($_POST["num"])? $_POST["num"]: "";
-        $building = isset($_POST["location"])? $_POST["location"]: "";
-        $institue = isset($_POST["institute"])? $_POST["institute"]: "";
-        $capacity = isset($_POST["capacity"])? $_POST["capacity"]: "";
-        $hfrom = isset($_POST["timestart"])? $_POST["timestart"]: "";
-        $hto = isset($_POST["timeend"])? $_POST["timeend"]: "";
+    <?php
+    //info for adding to db
+    $rnumber = isset($_POST["num"])? $_POST["num"]: "";
+    $building = isset($_POST["location"])? $_POST["location"]: "";
+    $institue = $_SESSION['inst'];
+    $capacity = isset($_POST["capacity"])? $_POST["capacity"]: "";
+    $hfrom = isset($_POST["timestart"])? $_POST["timestart"]: "";
+    $hto = isset($_POST["timeend"])? $_POST["timeend"]: "";
 
 
-//connect to the database
-        include("includes/config.php");
-        include("includes/db.php");
-        if ($db->connect_error) die("Connection failed: " . $db->connect_error);//FIXME only show error during debugging
+    //connect to the database
+    include("includes/config.php");
+    include("includes/db.php");
+    if ($db->connect_error) die("Connection failed: " . $db->connect_error);//FIXME only show error during debugging
 
 
-        $sql = "INSERT INTO `rooms` (`id`, `roomNumber`, `building`, `institute`, `capacity`, `hoursAvailableS`, `hoursAvailableE`) VALUES (NULL, '$rnumber', '$building', '$institue', '$capacity', '$hfrom', '$hto' )";
+    $sql = "INSERT INTO `rooms` (`id`, `roomNumber`, `building`, `institute`, `capacity`, `hoursAvailableS`, `hoursAvailableE`) VALUES (NULL, '$rnumber', '$building', '$institue', '$capacity', '$hfrom', '$hto' )";
 
-        echo "<p></p>";
+    echo "<p></p>";
 
-        if ($db->query($sql) === TRUE) {
-            echo "Room " . $rnumber . " in " . $institue . " has been added!";
-        }
+    if ($db->query($sql) === TRUE) {
+        echo "Room " . $rnumber . " in " . $institue . " has been added!";
+    }
 
-        $db->close();
-?>
+    $db->close();
+    ?>
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/bootstrap-clockpicker.min.js"></script>
