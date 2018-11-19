d3ce9593f533be073f60921d9485a6123c62faee
Steve <steven.brown.2016@uni.strath.ac.uk>
2018-11-19 19:34:19 +0000

    FIxed remove rooms/bookings I think

--- removeBookings.php
+++ removeBookings.php
@@ -6,11 +6,11 @@
 <body>
 <div>
     Are you sure you want to delete this booking?
-    <form action="removeBookings.php" method="post">
-        <?php $chosenBooking = $_POST["id"]; ?>
-        <td><button type=\"submit\" name = \"confirm\" value = <?php echo $chosenBooking; ?>>Yes</button></td>
-        <td><button type=\"submit\" name = \"confirm\" value = no>No</button></td>
-        
+    <form method="post">
+        <?php $chosenBooking = 5; ?>
+        <td><button type="submit" name = "confirm" value = yes>Yes</button></td>
+        <td><button type="submit" name = "confirm" value = no>No</button></td>
+
         <?php
         // connect to mysql
         $host = "devweb2018.cis.strath.ac.uk";
@@ -18,25 +18,25 @@
         $pass = "aeCh1ang9ahm";
         $dbname = "cs312groupk";
         $conn = new mysqli($host, $user, $pass, $dbname);
-        
+
         if ($conn->connect_error)
         {
             die("Connection failed : ".$conn->connect_error); // Remove once working!!!
         }
-        
-        $chosenBookingConfirmed = $_POST["confirm"];
-        
-        if($chosenBookingConfirmed != "no")
+
+        $confirmed = $_POST["confirm"];
+        if($confirmed === "yes")
         {
             // Issue the query
-            $sql = "DELETE * FROM `bookings` WHERE id = '$chosenBookingConfirmed'";
+            //$sql = "DELETE * FROM `bookings` WHERE id = '$confirmed'";
+            echo "If you see this then that means the confirmation works. Just need to uncomment the delete statement";
         }
-        
+
         // Disconnect
         $conn->close();
-        
-        header("Location: https://devweb2018.cis.strath.ac.uk/~gxb16190/groupk/index.php");
-        exit();
+
+        //header("Location: https://devweb2018.cis.strath.ac.uk/~gxb16190/groupk/index.php");
+        //exit();
         ?>
 </div>
 </body>
--- removeRooms.php
+++ removeRooms.php
@@ -5,11 +5,39 @@
     <h1>Title</h1>
 <body>
 <div>
-  Are you sure you want to delete this room?
-    <form action="deletingRoom.php" method="post">
-        <?php $chosenRoom = $_POST["id"]; ?>
-        <td><button type=\"submit\" name = \"confirm\" value = <?php echo $chosenRoom; ?>>Yes</button></td>
-        <td><button type=\"submit\" name = \"confirm\" value = no>No</button></td>
+    Are you sure you want to delete this room?
+    <form method="post">
+        <?php $chosenBooking = 5; ?>
+        <td><button type="submit" name = "confirm" value = yes>Yes</button></td>
+        <td><button type="submit" name = "confirm" value = no>No</button></td>
+
+        <?php
+        // connect to mysql
+        $host = "devweb2018.cis.strath.ac.uk";
+        $user = "cs312groupk";
+        $pass = "aeCh1ang9ahm";
+        $dbname = "cs312groupk";
+        $conn = new mysqli($host, $user, $pass, $dbname);
+
+        if ($conn->connect_error)
+        {
+            die("Connection failed : ".$conn->connect_error); // Remove once working!!!
+        }
+
+        $confirmed = $_POST["confirm"];
+        if($confirmed === "yes")
+        {
+            // Issue the query
+            //$sql = "DELETE * FROM `rooms` WHERE id = '$confirmed'";
+            echo "If you see this then that means the confirmation works. Just need to uncomment the delete statement";
+        }
+
+        // Disconnect
+        $conn->close();
+
+        //header("Location: https://devweb2018.cis.strath.ac.uk/~gxb16190/groupk/index.php");
+        //exit();
+        ?>
 </div>
 </body>
 </html>
