c132b389c3b272f83a27fdc2c5358a430e5f988d
Julia Tennant CS2016 <julia.tennant.2016@uni.strath.ac.uk>
2018-11-23 17:50:17 +0000

    Refactored owner home, just missing remove rooms

--- ownerhome.php
+++ ownerhome.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Home </title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width = device-width, initial-scale = 1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Homepage</title>
 
-
-
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
     <?php
     session_start();
     if($_SESSION['user-type'] === 'normal')
@@ -14,112 +15,102 @@
         header("location:userhome.php");
     }
     include("includes/header.php");
+    $name = $_SESSION['name'];
+    $email = $_SESSION['email'];
+    $inst = $_SESSION['inst'];
+
+    include("includes/config.php");
+    include("includes/db.php");
+
+    if ($db->connect_error) die("Connection failed: " . $db->connect_error);
+
+
+
+    $result = mysqli_query($db, "SELECT * FROM rooms WHERE institute = '$inst'");
+
+    $result2 = mysqli_query($db, "SELECT b.*, r.roomNumber FROM bookings b JOIN rooms r ON (r.id = b.room_id) WHERE b.institution = '$inst'");
     ?>
     <h1>Home lmao </h1>
+
 <body>
-<?php
-/**
+<div class="container">
+<?php /**
  * Created by IntelliJ IDEA.
  * User: rnb16141
  * Date: 14/11/2018
  * Time: 14:23
- */
+ */?>
 
+ <?php if($result->num_rows == 0) { ?>
+    <div class="alert alert-primary" role="alert">
+        Your institution hasn't added any rooms yet!
+        <form action = 'addroom.php' method = 'post'>
+            <button name ='add' class='btn btn-outline-primary' /> Add Room</button></form>
+    </div>
 
-$name = $_SESSION['name'];
-$email = $_SESSION['email'];
-$inst = $_SESSION['inst'];
+ <?php } else { ?>
 
-
-include("includes/config.php");
-include("includes/db.php");
-
-
-if ($db->connect_error) die("Connection failed: " . $db->connect_error);
-
-
-
-// Issue the query
-$sql = "SELECT * FROM `rooms` WHERE `institute` = '$inst'";
-$result = $db->query($sql);
-
-
-if ($result->num_rows > 0) {
-    echo "
-    <table>
+    <table class="table">
+            <thead>
+            <tr>
+                <th>Room Number </th>
+                <th>Building </th>
+                <th>Institute </th>
+                <th>Capacity </th>
+                <th>Hours Available </th>
+            </tr>
+            </thead>
+             <tbody>
+     <?php while ($row = $result->fetch_assoc()) { ?>
         <tr>
-            <th></th>
-            <th>Room Number </th>
-            <th>Building </th>
-            <th>Institute </th>
-            <th>Capacity </th>
-            <th>Hours Available </th>
+        <td> <?php echo  $row["roomNumber"]?> </td>
+        <td><?php echo  $row["building"]?> </td>
+        <td> <?php echo  $row["institute"]?> </td>
+        <td><?php echo  $row["capacity"]?> </td>
+        <td><?php echo  $row["hoursAvailableS"]?>  - <?php echo $row["hoursAvailableE"]?> </td>
+       <form action = 'removeBooking.php' value = <?php $row["id"]?> method = 'post'>
+        <td><button name ='remove' class='btn btn-outline-primary'  value=<?php echo $row["id"]; ?>/> Remove Room</button></td></form>";
+
         </tr>
-  ";
-    // Filling the table
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>\n";
-        echo "<td></td>";
-        echo "<td>" . $row["roomNumber"] . "</td>";
-        echo "<td>" . $row["building"] . "</td>";
-        echo "<td>" . $row["institute"] . "</td>";
-        echo "<td>" . $row["capacity"] . "</td>";
-        echo "<td>" . $row["hoursAvailableS"] . " - " . $row["hoursAvailableE"] . "</td>";
-        echo "<form action = 'removeBooking.php' value =" . $row["id"]. " method = 'post'>";
-        echo "<td><button name ='remove' class='btn btn-outline-primary'  value=". $row["id"] . "/> Remove Room</button></td></form>";
-
-
-
-        echo "</tr>\n";
-    }
-}
-else{
-    echo "You have no rooms currently added by your institution!";
-}
-
-echo "<form action = 'addroom.php' method = 'post'>";
-echo "<td><button name ='add' class='btn btn-outline-primary' /> Add Room</button></td></form>";
-
-//$sql2 = "SELECT bookings.*, rooms.roomNumber FROM `bookings` JOIN `rooms` WHERE `rooms.institution` = '$inst'";
-$sql2 = "SELECT bookings.*, rooms. FROM `bookings` JOIN `rooms` ON (bookings.room_id = rooms.id)  WHERE `bookings.institute` = '$inst' AND `rooms.institution` = '$inst'";
-$result2 = $db->query($sql2);
-
-if ($result2->num_rows > 0) {
-    echo "
-    <table>
+     <?php } ?>
+     </tbody>
+    </table>
+ <?php } ?>
+<div></div>
+<?php if($result2->num_rows == 0) { ?>
+    <div class="alert alert-primary" role="alert">
+        No bookings have been made for your rooms yet!
+    </div>
+<?php } else { ?>
+    <table class="table">
+        <thead>
         <tr>
-            <th></th>
             <th>Meeting Name</th>
             <th>Time</th>
             <th>Date</th>
             <th>Room No.</th>
         </tr>
-  ";
-    // Filling the table
-    while ($row = $result2->fetch_assoc()) {
-        echo "<tr>\n";
-        echo "<td></td>";
-        echo "<td>" . $row["meeting_name"] . "</td>";
-        echo "<td>" . $row["start_time"]." - ".$row["end_time"]. "</td>";
-        echo "<td>" . $row["meeting_date"] . "</td>";
-        echo "<td>" . $row["roomNumber"] . "</td>";
+        </thead>
+        <tbody>
+        <?php while ($row = $result2->fetch_assoc()) { ?>
+            <tr>
+                <td> <?php echo $row["meeting_name"]?> </td>
+                <td><?php echo  $row["start_time"]?> - <?php echo $row["end_time"]?></td>
+                <td><?php echo  $row["meeting_date"]?> </td>
+                <td><?php echo   $row["roomNumber"]?> </td>
 
-        echo "</tr>\n";
-    }
-}
-else{
-    echo "No bookings have been made for your rooms yet!";
-}
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+<?php } ?>
 
-// Disconnect
-$db->close();
 
-?>
+</div>
+
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 
 
 </body>
 </html>
-
-
