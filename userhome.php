96586efc909278fa07a9d56954e7022cc19d0cfb
Julia Tennant CS2016 <julia.tennant.2016@uni.strath.ac.uk>
2018-11-23 17:52:40 +0000

    Update userhome.php

--- userhome.php
+++ userhome.php
@@ -1,28 +1,3 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: rnb16141
- * Date: 14/11/2018
- * Time: 14:22
- */
-
-
-include("includes/config.php");
-include("includes/db.php");
-
-session_start();
-if($_SESSION['user-type'] === 'owner')
-{
-    header("location:ownerhome.php");
-}
-
-
-$id = $_SESSION['id'];
-$name = $_SESSION['name'];
-$email = $_SESSION['email'];
-
-$result = mysqli_query($db, "SELECT b.*, r.roomNumber FROM bookings b JOIN rooms r ON (r.id = b.room_id) WHERE b.user_id = '$id'");
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,45 +7,110 @@ $result = mysqli_query($db, "SELECT b.*, r.roomNumber FROM bookings b JOIN rooms
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Homepage</title>
-</head>
+
+    <?php
+    session_start();
+    if($_SESSION['user-type'] === 'normal')
+    {
+        header("location:userhome.php");
+    }
+
+    $name = $_SESSION['name'];
+    $email = $_SESSION['email'];
+    $inst = $_SESSION['inst'];
+    include("includes/header.php");
+    include("includes/config.php");
+    include("includes/db.php");
+
+    if ($db->connect_error) die("Connection failed: " . $db->connect_error);
+
+
+
+    $result = mysqli_query($db, "SELECT * FROM rooms WHERE institute = '$inst'");
+
+    $result2 = mysqli_query($db, "SELECT b.*, r.roomNumber FROM bookings b JOIN rooms r ON (r.id = b.room_id) WHERE b.institution = '$inst'");
+    ?>
+    <h1>Home lmao </h1>
+
 <body>
-<?php include("includes/header.php"); ?>
 <div class="container">
-    <?php if($result->num_rows == 0) { ?>
-        <div class="alert alert-primary" role="alert">
-            You haven't made any bookings yet.
-            <form action = 'bookroom.php' method = 'post'>
-            <button name ='book' class='btn btn-outline-primary' /> Book a Room!</button></form>
-        </div>
-    <?php } else { ?>
-        <table class="table">
+<?php /**
+ * Created by IntelliJ IDEA.
+ * User: rnb16141
+ * Date: 14/11/2018
+ * Time: 14:23
+ */?>
+
+ <?php if($result->num_rows == 0) { ?>
+    <div class="alert alert-primary" role="alert">
+        Your institution hasn't added any rooms yet!
+        <form action = 'addroom.php' method = 'post'>
+            <button name ='add' class='btn btn-outline-primary' /> Add Room</button></form>
+    </div>
+
+ <?php } else { ?>
+
+    <table class="table">
             <thead>
             <tr>
-                <th>Meeting Name</th>
-                <th>Time</th>
-                <th>Date</th>
-                <th>Institution</th>
-                <th>Room No.</th>
+                <th>Room Number </th>
+                <th>Building </th>
+                <th>Institute </th>
+                <th>Capacity </th>
+                <th>Hours Available </th>
             </tr>
             </thead>
-            <tbody>
-            <?php while ($row = $result->fetch_assoc()) { ?>
-                <tr>
-                    <td><?php echo $row["meeting_name"]; ?></td>
-                    <td><?php echo $row["start_time"]." - ".$row["end_time"]; ?></td>
-                    <td><?php echo $row["meeting_date"]; ?></td>
-                    <td><?php echo $row["institution"]; ?></td>
-                    <td><?php echo $row["roomNumber"]; ?></td>
-                    <form action = 'removeBooking.php' value = <?php echo $row["booking_id"]; ?> method = 'post'>
-                    <td><button name ='remove' class='btn btn-outline-primary'  value=<?php echo $row["booking_id"]; ?>/> Remove Room</button></td></form>;
+             <tbody>
+     <?php while ($row = $result->fetch_assoc()) { ?>
+        <tr>
+        <td> <?php echo  $row["roomNumber"]?> </td>
+        <td><?php echo  $row["building"]?> </td>
+        <td> <?php echo  $row["institute"]?> </td>
+        <td><?php echo  $row["capacity"]?> </td>
+        <td><?php echo  $row["hoursAvailableS"]?>  - <?php echo $row["hoursAvailableE"]?> </td>
+       <form action = 'removeBooking.php' value = <?php $row["id"]?> method = 'post'>
+        <td><button name ='remove' class='btn btn-outline-primary'  value=<?php echo $row["id"]; ?>/> Remove Room</button></td></form>";
 
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
-    <?php } ?>
+        </tr>
+     <?php } ?>
+     </tbody>
+    </table>
+ <?php } ?>
+<div></div>
+<?php if($result2->num_rows == 0) { ?>
+    <div class="alert alert-primary" role="alert">
+        No bookings have been made for your rooms yet!
+    </div>
+<?php } else { ?>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Meeting Name</th>
+            <th>Time</th>
+            <th>Date</th>
+            <th>Room No.</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php while ($row = $result2->fetch_assoc()) { ?>
+            <tr>
+                <td> <?php echo $row["meeting_name"]?> </td>
+                <td><?php echo  $row["start_time"]?> - <?php echo $row["end_time"]?></td>
+                <td><?php echo  $row["meeting_date"]?> </td>
+                <td><?php echo   $row["roomNumber"]?> </td>
+
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+<?php } ?>
+
+
 </div>
 
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+
+
 </body>
+</html>
