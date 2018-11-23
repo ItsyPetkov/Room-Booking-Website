6cdfa72cf97965d9ecd2dd7e0cd760af857e9185
Steve <steven.brown.2016@uni.strath.ac.uk>
2018-11-23 20:59:28 +0000

    Buggered up details by accident, fixed now

--- details.php
+++ details.php
@@ -87,112 +87,112 @@ $name = $_SESSION['name'];
     </div>
 </div>
 
-        <div class="modal fade" role="dialog" id="editEmailPage" tabindex="-1">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h6 class="modal-title">Edit Details</h6>
+<div class="modal fade" role="dialog" id="editEmailPage" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title">Edit Details</h6>
+            </div>
+            <div class="modal-body">
+                <form action="details.php" method="post">
+                    <div class="form-group">
+                        New Email: <input type="text" class="form-control" name="newmail" placeholder="Enter new email"/>
                     </div>
-                    <div class="modal-body">
-                        <form action="details.php" method="post">
-                            <div class="form-group">
-                                New Email: <input type="text" class="form-control" name="newmail" placeholder="Enter new email"/>
-                            </div>
-                            <div class="form-group">
-                                Confirm Email: <input type="text" class="form-control" name="confmail" placeholder="Confirm your email"/>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="submit" name="editemail" class="btn btn-outline-primary">Edit</button>
-                                <button class="btn btn-outline-primary" data-dismiss="modal">Cancel</button>
-                            </div>
-                        </form>
+                    <div class="form-group">
+                        Confirm Email: <input type="text" class="form-control" name="confmail" placeholder="Confirm your email"/>
                     </div>
-                </div>
+                    <div class="modal-footer">
+                        <button type="submit" name="editemail" class="btn btn-outline-primary">Edit</button>
+                        <button class="btn btn-outline-primary" data-dismiss="modal">Cancel</button>
+                    </div>
+                </form>
             </div>
         </div>
-
-        <div class="modal fade" role="dialog" id="editInstitutionPage" tabindex="-1">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h6 class="modal-title">Edit Details</h6>
-                    </div>
-                    <div class="modal-body">
-                        <form action="details.php" method="post">
-                            <div class="form-group">
-                                New Institution: <input type="text" class="form-control" name="newInstitution" placeholder="Enter new Institution"/>
-                            </div>
-                            <div class="form-group">
-                                Confirm Institution: <input type="text" class="form-control" name="confInstitution" placeholder="Confirm your Institution"/>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="submit" name="editInstitution" class="btn btn-outline-primary">Edit</button>
-                                <button class="btn btn-outline-primary" data-dismiss="modal">Cancel</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <div>
-    <?php
-        if(isset($_POST['editname']))
-        {
-            $newName = isset($_POST["newname"])? mysqli_real_escape_string($db, $_POST["newname"]): "";
-            $confName = isset($_POST["conname"])? mysqli_real_escape_string($db, $_POST["conname"]): "";
-
-            if($newName === $confName){
-                $sql = "UPDATE `cs312groupk`.`users` SET `name` = '$confName' WHERE `id` = '$id'";
-                $result = $db->query($sql);
-                if($result){
-                    echo "<p style='text-align: center'>Name changed successfully, please refresh page to view results</p>";
-                    $_SESSION['name'] = $confName;
-                }else{
-                    echo "<p style='text-align: center'>Change failed</p>";
-                }
-            }else{
-                echo "<p style='text-align: center'>Names don't match, please try again</p>";
-            }
-        }
-
-        elseif(isset($_POST['editemail'])){
-
-            $newEmail = isset($_POST["newmail"])? mysqli_real_escape_string($db, $_POST["newmail"]): "";
-            $confEmail = isset($_POST["confmail"])? mysqli_real_escape_string($db, $_POST["confmail"]): "";
-
-            if(($newEmail === $confEmail) && filter_var($confEmail, FILTER_VALIDATE_EMAIL)){
-                $sql3 = "UPDATE `cs312groupk`.`users` SET `email` = '$confEmail' WHERE `id` = '$id'";
-                $result3 = $db->query($sql3);
-                if($result3){
-                    echo "<p style='text-align: center'>Email changed successfully, please refresh page to view results</p>";
-                    $_SESSION['email'] = $confEmail;
-                }else{
-                    echo "<p style='text-align: center'>Change failed</p>";
-                }
-            }else{
-                echo "<p style='text-align: center'>Emails don't match or are not valid, please try again</p>";
-            }
-        }
-
-        elseif(isset($_POST['editInstitution'])){
-            $newInstitution = isset($_POST['newInstitution'])? mysqli_real_escape_string($db, $_POST['newInstitution']): "";
-            $confInstitution = isset($_POST['confInstitution'])? mysqli_real_escape_string($db, $_POST['confInstitution']): "";
-
-            if($newInstitution === $confInstitution){
-                $sql4 = "UPDATE `cs312groupk`.`users` SET `institute` = '$confInstitution' WHERE `id` = '$id'";
-                $result4 = $db->query($sql4);
-                if($result4){
-                    echo "<p style='text-align: center'>Institution changed successfully, please refresh page to view results</p>";
-                    $_SESSION['institute'] = $confInstitution;
-                }else{
-                    echo "<p style='text-align: center'>Change failed</p>";
-                }
-            }else{
-                echo "<p style='text-align: center'>Institutions don't match, please try again</p>";
-            }
-        }
-    ?>
     </div>
+</div>
+
+<div class="modal fade" role="dialog" id="editInstitutionPage" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title">Edit Details</h6>
+            </div>
+            <div class="modal-body">
+                <form action="details.php" method="post">
+                    <div class="form-group">
+                        New Institution: <input type="text" class="form-control" name="newInstitution" placeholder="Enter new Institution"/>
+                    </div>
+                    <div class="form-group">
+                        Confirm Institution: <input type="text" class="form-control" name="confInstitution" placeholder="Confirm your Institution"/>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" name="editInstitution" class="btn btn-outline-primary">Edit</button>
+                        <button class="btn btn-outline-primary" data-dismiss="modal">Cancel</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+    <?php
+    if(isset($_POST['editname']))
+    {
+        $newName = isset($_POST["newname"])? mysqli_real_escape_string($db, $_POST["newname"]): "";
+        $confName = isset($_POST["conname"])? mysqli_real_escape_string($db, $_POST["conname"]): "";
+
+        if($newName === $confName){
+            $sql = "UPDATE `cs312groupk`.`users` SET `name` = '$confName' WHERE `id` = '$id'";
+            $result = $db->query($sql);
+            if($result){
+                echo "<p style='text-align: center'>Name changed successfully, please refresh page to view results</p>";
+                $_SESSION['name'] = $confName;
+            }else{
+                echo "<p style='text-align: center'>Change failed</p>";
+            }
+        }else{
+            echo "<p style='text-align: center'>Names don't match, please try again</p>";
+        }
+    }
+
+    elseif(isset($_POST['editemail'])){
+
+        $newEmail = isset($_POST["newmail"])? mysqli_real_escape_string($db, $_POST["newmail"]): "";
+        $confEmail = isset($_POST["confmail"])? mysqli_real_escape_string($db, $_POST["confmail"]): "";
+
+        if(($newEmail === $confEmail) && filter_var($confEmail, FILTER_VALIDATE_EMAIL)){
+            $sql3 = "UPDATE `cs312groupk`.`users` SET `email` = '$confEmail' WHERE `id` = '$id'";
+            $result3 = $db->query($sql3);
+            if($result3){
+                echo "<p style='text-align: center'>Email changed successfully, please refresh page to view results</p>";
+                $_SESSION['email'] = $confEmail;
+            }else{
+                echo "<p style='text-align: center'>Change failed</p>";
+            }
+        }else{
+            echo "<p style='text-align: center'>Emails don't match or are not valid, please try again</p>";
+        }
+    }
+
+    elseif(isset($_POST['editInstitution'])){
+        $newInstitution = isset($_POST['newInstitution'])? mysqli_real_escape_string($db, $_POST['newInstitution']): "";
+        $confInstitution = isset($_POST['confInstitution'])? mysqli_real_escape_string($db, $_POST['confInstitution']): "";
+
+        if($newInstitution === $confInstitution){
+            $sql4 = "UPDATE `cs312groupk`.`users` SET `institute` = '$confInstitution' WHERE `id` = '$id'";
+            $result4 = $db->query($sql4);
+            if($result4){
+                echo "<p style='text-align: center'>Institution changed successfully, please refresh page to view results</p>";
+                $_SESSION['institute'] = $confInstitution;
+            }else{
+                echo "<p style='text-align: center'>Change failed</p>";
+            }
+        }else{
+            echo "<p style='text-align: center'>Institutions don't match, please try again</p>";
+        }
+    }
+    ?>
+</div>
 </div>
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
