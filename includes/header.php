da9898884d298ee386797724add45279ae7d4038
Slav Ivov Ivanov CS2016 <slav.ivanov.2016@uni.strath.ac.uk>
2018-11-25 18:04:34 +0000

    Fixed header issue not displaying properly on addroom.php

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,7 @@
             <?php } ?>
 		</ul>
 		<span style="margin-right: 10px" class="navbar-text">
-			Hi, <?php echo $name; ?>
+			Hi, <?php echo $_SESSION['name']; ?>
 		</span>
         <a style="margin-right: 5px" class="btn btn-outline-info my-2 my-sm-0" href="details.php">Your Details</a>
 		<a style="margin-right: 10px" class="btn btn-outline-secondary my-2 my-sm-0" href="logout.php">Log out</a>
